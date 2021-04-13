29c2e7d9cfb2491c782b2d944dc619f695e76bdb
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-04-13 20:46:15 -0300

    usuario entidade e repositório

---
+++ src/main/java/com/broadfactor/entidade/UsuarioEntidade.java
@@ -0,0 +1,57 @@
+package com.broadfactor.entidade;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity @Table(name="tb_usuario")
+public class UsuarioEntidade {
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id_usuario;
+	
+	@NotNull
+	private String email;
+	
+	@NotNull
+	private String senha;
+	
+	@NotNull
+	private String nome;
+
+	public long getId_usuario() {
+		return id_usuario;
+	}
+
+	public void setId_usuario(long id_usuario) {
+		this.id_usuario = id_usuario;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
