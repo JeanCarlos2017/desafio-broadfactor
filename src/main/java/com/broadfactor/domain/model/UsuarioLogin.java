1b11117ef9dd12195dd2fb50fd9aa0e7d5dc1097
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-04-13 21:02:16 -0300

    Create UsuarioLogin.java

---
+++ src/main/java/com/broadfactor/domain/model/UsuarioLogin.java
@@ -0,0 +1,61 @@
+package com.broadfactor.domain.model;
+
+
+public class UsuarioLogin {
+	private long id;
+	
+	private String email;
+	
+	private String nome;
+	
+	private String token;
+	
+	private String senha;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+	
+	
+
+	
+	
+}
